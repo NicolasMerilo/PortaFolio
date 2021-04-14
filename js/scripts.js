6bc59fa44bb2d97c9a8eb7a29de276d68161c2ed
NicolasMerilo <65987209+NicolasMerilo@users.noreply.github.com>
2021-04-14 19:35:19 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
+var hr = document.querySelector("#hr");
+var elements = document.querySelectorAll("#text");
 var links = document.querySelectorAll(".nav-link");
+var btn = document.getElementById('navbarNav');
+
+document.addEventListener('DOMContentLoaded', function() {
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+
   for (var i = 0; i < links.length; i++) {
     var link = links[i];
     link.onclick = function() {
@@ -10,23 +20,18 @@ var links = document.querySelectorAll(".nav-link");
     };
   }
 
-var hr = document.querySelector("#hr");
-var elements = document.querySelectorAll("#text");
-setTimeout(() => { 
-for (i = 0; i < elements.length; i++) {
-    elements[i].classList.remove('hidden');
-}
-hr.classList.remove('hrh');
-}, 1500);
+  setTimeout(() => { 
+  for (i = 0; i < elements.length; i++) {
+      elements[i].classList.remove('hidden');
+  }
+  hr.classList.remove('hrh');
+  }, 1500);
 
-var btn = document.getElementById('navbarNav');
-btn.onclick = function() {
- btn.className +="hidden";
-}
-
-function toogle(){
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
 })
-}
+
+ 
+
+btn.addEventListener("click", function () {
+  btn.className ="navbar-collapse justify-content-end collapse";
+})
+
